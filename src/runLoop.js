13945355830deadb2483322280f09a77f51e5c24
Jia Rong <jr.loves.maths@gmail.com>
2018-08-11 18:47:42 +0800

    feat: add rescue retry on Invalid signature

--- src/runLoop.js
+++ src/runLoop.js
@@ -4,6 +4,13 @@ import * as act from './lib'
 
 const createOrder = async (...params) => act.orders.create(...params)
 const cancelAllOpenOrders = async (...params) => act.orders.cancelAllOpen(...params)
+const clearOpenOrdersForAccounts = async (switcheo, accounts) => {
+  // Cancel all existing open orders
+  for (const account of accounts) {
+    // eslint-disable-next-line no-await-in-loop
+    await cancelAllOpenOrders({ switcheo, account })
+  }
+}
 
 const printOrders = (account, orders) => {
   orders.forEach(order => {
@@ -24,43 +31,54 @@ const createOrderParams = (side, flip, buyParams, sellParams) =>
 
 // Main logic for bot
 const runLoop = async (switcheo, accounts, config, runnerConfig = {}) => {
-  let res
   const { flipCreateParams = false, hasClearedInitialOpenOrders = false } = runnerConfig
   const { interval = 1000, orders } = cloneDeep(config)
-  const { priceSteps } = orders.create
 
-  const createOrdersBuy = orders.create.buyParams
-  const createOrdersSell = orders.create.sellParams
+  try {
+    let res
+    const { priceSteps } = orders.create
 
-  const createOrderOptions = { num: 20, priceSteps }
+    const createOrdersBuy = orders.create.buyParams
+    const createOrdersSell = orders.create.sellParams
+    const createOrderOptions = { num: 2, priceSteps }
 
-  if (!hasClearedInitialOpenOrders) {
-    // Cancel all existing open orders
-    for (const account of accounts) {
-      // eslint-disable-next-line no-await-in-loop
-      await cancelAllOpenOrders({ switcheo, account })
+    if (!hasClearedInitialOpenOrders) await clearOpenOrdersForAccounts(switcheo, accounts)
+
+
+    // res = await createOrder({ switcheo, account: accounts[0] },
+    //   ...createOrderParams('buy', flipCreateParams, createOrdersBuy, createOrdersSell), createOrderOptions)
+    // printOrders(account, res)
+    // await sleep(1)
+    //
+    // res = await createOrder({ switcheo, account: accounts[1] },
+    //   ...createOrderParams('sell', flipCreateParams, createOrdersBuy, createOrdersSell), createOrderOptions)
+    // printOrders(account, res)
+    // await sleep(1)
+
+
+    res = await createOrder({ switcheo, account: accounts[0] },
+      ...createOrderParams('buy', flipCreateParams, createOrdersBuy, createOrdersSell),
+      { ...createOrderOptions, parallel: true }) // ideally, this account will always do makes
+    printOrders(accounts[0], res)
+
+    res = await createOrder({ switcheo, account: accounts[1] },
+      ...createOrderParams('sell', flipCreateParams, createOrdersBuy, createOrdersSell), createOrderOptions)
+    printOrders(accounts[1], res) // ideally, this account will always do fills
+  } catch (err) {
+    const invalidSigMessage = 'Invalid signature'
+
+    // ocassionally, verify_sig function from switcheo-api fails, so we try to run again
+    if (err.message.includes(invalidSigMessage)) {
+      console.warn('invalidSigMessage')
+      await runLoop(switcheo, accounts, config, {
+        ...runnerConfig,
+        flipCreateParams, // don't flip, retry with same args again
+        hasClearedInitialOpenOrders: false, // reset orders
+      })
+    } else {
+      throw new Error(err)
     }
   }
-
-  // res = await createOrder({ switcheo, account: accounts[0] },
-  //   ...createOrderParams('buy', flipCreateParams, createOrdersBuy, createOrdersSell), createOrderOptions)
-  // printOrders(account, res)
-  // await sleep(1)
-  //
-  // res = await createOrder({ switcheo, account: accounts[1] },
-  //   ...createOrderParams('sell', flipCreateParams, createOrdersBuy, createOrdersSell), createOrderOptions)
-  // printOrders(account, res)
-  // await sleep(1)
-
-
-  res = await createOrder({ switcheo, account: accounts[0] },
-    ...createOrderParams('buy', flipCreateParams, createOrdersBuy, createOrdersSell),
-    { ...createOrderOptions, parallel: true }) // ideally, this account will always do makes
-  printOrders(accounts[0], res)
-
-  res = await createOrder({ switcheo, account: accounts[1] },
-    ...createOrderParams('sell', flipCreateParams, createOrdersBuy, createOrdersSell), createOrderOptions)
-  printOrders(accounts[1], res) // ideally, this account will always do fills
 
   // sleep and loop
   await sleep(interval)
